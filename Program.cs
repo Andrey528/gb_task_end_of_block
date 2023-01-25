6c76978bdb34c0d2e8998d7fc2e4e5db34b4ec5c
Andrew <telepnev.1997@mail.ru>
2023-01-25 11:45:32 +0700

    new realese

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
-﻿Console.WriteLine($"Пользователь вводит с клавиатуры строки. " +
-    $"Каждую строку пользователь разделяет через \n");
+﻿void Print_array(List<string> array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Count; i++)
+    {
+        if (i == array.Count - 1) 
+            Console.Write($"\"{array[i]}\"");
+        else 
+            Console.Write($"\"{array[i]}\", ");
+    }
+    Console.Write($"]");
+}
 
-string[] usrArray = Console.ReadLine()!.Split('\n');
+Console.WriteLine($"Пользователь вводит с клавиатуры строки. " +
+    $"Каждую строку пользователь разделяет через ,");
+
+string[] usrArray = Console.ReadLine()!.Split(',');
 List<string> newArray = new List<string>();
 int index2 = 0;
+int stringLength = 3;
+Random r = new Random();
 
 for (int index1 = 0; index1 < usrArray.Length; index1++)
 {
-    if ((new Random().Next(0, 1) == 1) && (index2 < 3))
+    if (usrArray[index1].Length <= stringLength)
     {
-
+        newArray.Add(usrArray[index1]);
+        index2++;
     }
 }
+
+Print_array(newArray);
