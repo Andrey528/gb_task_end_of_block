b556d07d7065e7e52422e917c3a579cf6e337db0
Andrew <telepnev.1997@mail.ru>
2023-01-25 11:20:17 +0700

    add a loop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Пользователь вводит с клавиатуры строки. " +
+    $"Каждую строку пользователь разделяет через \n");
+
+string[] usrArray = Console.ReadLine()!.Split('\n');
+List<string> newArray = new List<string>();
+int index2 = 0;
+
+for (int index1 = 0; index1 < usrArray.Length; index1++)
+{
+    if ((new Random().Next(0, 1) == 1) && (index2 < 3))
+    {
+
+    }
+}
